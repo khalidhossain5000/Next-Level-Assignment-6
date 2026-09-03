95a58c47dfc6eda9f3588cc3616696fe8fcc3eb6
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-09-03 20:31:00 +0600

    feat:redis setup added and auth service partially added

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,11 +1,27 @@
+import { prisma } from "../../lib/prisma";
+import { AppError } from "../../utils/AppError";
 import type { IRegisterUser } from "./auth.interface"
+import httpStatus from "http-status";
+import bcrypt from "bcryptjs";
 
-const registerUserInDb=async(payload:IRegisterUser)=>{
 
+const registerUserInDb = async (payload: IRegisterUser) => {
+    const { name, password, role } = payload
+    const email = payload.email.trim().toLowerCase();
+
+    const isUserExists = await prisma.user.findUnique({
+        where: { email },
+    });
+    if (isUserExists) throw new AppError(httpStatus.CONFLICT, "User already exists with this email");
+
+    //s-2 hashed the password
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    //redis store start
 }
 
 
 
-export const authServices={
-registerUserInDb
+export const authServices = {
+    registerUserInDb
 }
