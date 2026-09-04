42dbc3c2c2a554ed6f7a8ebe0e85a250e36bd5fa
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-09-04 17:50:26 +0600

    feat:zone get all route added

--- src/app/modules/distributionInfrastructure/zone/zone.route.ts
+++ src/app/modules/distributionInfrastructure/zone/zone.route.ts
@@ -8,4 +8,9 @@ import { upload } from "../../../lib/multer";
 const router = Router();
 //create zone
 router.post("/",auth(Role.ADMIN),upload.single("zoneImage"),ZoneController.createZone)
+
+//get all zone for public route
+router.get("/",ZoneController.getAllZone)
+
+
 export const ZoneRoutes = router;
