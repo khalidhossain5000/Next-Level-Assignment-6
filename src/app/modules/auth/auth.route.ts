d83ffa06136dd9332ca4f20affcf5d80dd1ee04e
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-09-04 10:26:04 +0600

    feat:get me protection is aded

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { authController } from "./auth.controller";
+import { auth } from "../../middlewares/checkAuth";
 
 const router = Router();
 
@@ -8,6 +9,6 @@ router.post("/verify-email",authController.verifyUserEmail)
 router.post("/login",authController.loginUser)
 router.post("/refresh-token", authController.refreshToken);
 router.post("/google-login", authController.googleLoginUser);
-router.get("/get-me", authController.getMe);
+router.get("/get-me",auth("CUSTOMER","CUSTOMER","TECHNICIAN"), authController.getMe);
 
 export const AuthRoutes = router;
