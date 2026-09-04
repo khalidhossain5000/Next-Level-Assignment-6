75a0377a896119b246af15fa3c8285c31d8008ea
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-09-04 18:38:55 +0600

    feat: update substation routes for public access and role-based details retrieval

--- src/app/modules/distributionInfrastructure/substation/substation.route.ts
+++ src/app/modules/distributionInfrastructure/substation/substation.route.ts
@@ -8,4 +8,17 @@ import { SubstationController } from "./substation.controller";
 const router = Router();
 
 router.post("/",auth(Role.ADMIN),validateRequest(substationValidation.createSubstationZodSchema),SubstationController.createSubstation)
+
+
+//get all substion public
+router.get("/",SubstationController.getAllSubstation)
+
+//get details protected need lgoin
+
+router.get("/substationId",auth(Role.CUSTOMER,Role.TECHNICIAN,Role.ADMIN),SubstationController.getSubstationDetails)
+
+
+
+
+
 export const SubstationRoutes = router;
