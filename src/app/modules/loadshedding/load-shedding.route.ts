3cd938ed1963701f71f584099b91cd1018a56e4b
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-09-04 21:49:13 +0600

    feat: update load shedding routes to use LoadSheddingController for creating and retrieving schedules

--- src/app/modules/loadshedding/load-shedding.route.ts
+++ src/app/modules/loadshedding/load-shedding.route.ts
@@ -3,17 +3,17 @@ import { auth } from "../../middlewares/checkAuth";
 import { Role } from "../../../generated/prisma/enums";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { loadSheddingValidation } from "./load-shedding.validation";
-import { LoadSheddingService } from "./load-shedding.service";
+import { LoadSheddingController } from "./load-shedding.controller";
 
 const router = Router();
 
 //--create load shedding schedule for admin only
 
-router.post("/",auth(Role.ADMIN),validateRequest(loadSheddingValidation.createLoadSheddingZodSchema),LoadSheddingService.createLoadSheddingScheduleInDb)
+router.post("/",auth(Role.ADMIN),validateRequest(loadSheddingValidation.createLoadSheddingZodSchema),LoadSheddingController.createLoadShedding)
 
 //--get all load shedding scheudle this will be public route
 
-
+router.get("/",LoadSheddingController.getAllLoadShedding)
 
 //--details load shedding public
 
