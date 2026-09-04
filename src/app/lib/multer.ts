d76661603011cae15a5acbf96ce1d641ddf3ddae
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-09-04 13:08:50 +0600

    fix: multer file type removed will add later

--- src/app/lib/multer.ts
+++ src/app/lib/multer.ts
@@ -3,20 +3,6 @@ import multer from "multer";
 
 const storage = multer.memoryStorage();
 
-export const upload = multer({
-    storage, limits: {
-        fileSize: 5 * 1024 * 1024, // 5 MB
-    }, fileFilter: (req, file, cb) => {
-        const allowedMimeTypes = [
-            "application/pdf",
-            "application/msword",
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        ];
-
-        if (allowedMimeTypes.includes(file.mimetype)) {
-            cb(null, true);
-        } else {
-            cb(new Error("Only PDF, DOC and DOCX files are allowed"));
-        }
-    },
-});
+export const upload = multer({ storage,  limits: {
+    fileSize: 5 * 1024 * 1024, // 5 MB
+  }, });
