e5a99567d118395ffa787c8df2b2ab3ebbf54811
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-09-04 19:01:14 +0600

    feat: update area routes to use AreaController methods for area management

--- src/app/modules/distributionInfrastructure/area/area.route.ts
+++ src/app/modules/distributionInfrastructure/area/area.route.ts
@@ -2,20 +2,22 @@ import { Router } from "express";
 import { Role } from "../../../../generated/prisma/enums";
 import { auth } from "../../../middlewares/checkAuth";
 import { validateRequest } from "../../../middlewares/validateRequest";
+import { areaValidation } from "./area.validation";
+import { AreaController } from "./area.controller";
 
 
 const router = Router();
 
 
-router.post("/",auth(Role.ADMIN),validateRequest(feederValidation.createFeederZodSchema),FeederController.createFeeder)
+router.post("/",auth(Role.ADMIN),validateRequest(areaValidation.createAreaZodSchema),AreaController.createArea)
 
 
 //get all substion public
-router.get("/",FeederController.getAllFeeder)
+router.get("/",AreaController.getAllArea)
 
 //get details protected need lgoin
 
-router.get("/:areaId",auth(Role.CUSTOMER,Role.TECHNICIAN,Role.ADMIN),FeederController.getFeederDetails)
+router.get("/:areaId",auth(Role.CUSTOMER,Role.TECHNICIAN,Role.ADMIN),AreaController.getAreaDetails)
 
 
 
