9b2675df6e5760190d5f493a8403de18e0fe3347
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-09-04 18:33:16 +0600

    feat: substtion get all added with search sort pagination

--- src/app/modules/distributionInfrastructure/substation/substation.service.ts
+++ src/app/modules/distributionInfrastructure/substation/substation.service.ts
@@ -1,3 +1,5 @@
+import { SubstationWhereInput } from "../../../../generated/prisma/models"
+import { IQuery } from "../../../interfaces/interface"
 import { prisma } from "../../../lib/prisma"
 import type { ISubstationPayload } from "./substation.interface"
 
@@ -23,8 +25,84 @@ return createdSubstationResult
 
 }
 
+//get all substion ( not adding  filter)
 
+const getAllSubstationFromDb=async(query:IQuery)=>{
+     const limit = query.limit ? Number(query.limit) : 10;
+    const page = query.page ? Number(query.page) : 1;
+    const skip = (page - 1) * limit;
+    const sortBy = query.sortBy ? query.sortBy : "createdAt";
+    const sortOrder = query.sortOrder ? query.sortOrder : "desc"
+
+
+    
+        const andConditions: SubstationWhereInput[] = []
+    
+    
+        //Searching
+        if (query.searchTerm) {
+            andConditions.push({
+                OR: [
+                    { name: { contains: query.searchTerm, mode: "insensitive" } },
+                    { code: { contains: query.searchTerm, mode: "insensitive" } },
+                    {
+                        location: {
+                            contains: query.searchTerm,
+                            mode: "insensitive",
+                        },
+                    },
+    
+                ],
+            });
+        }
+
+
+
+    const allSubstation = await prisma.substation.findMany({
+        where: {
+            AND: andConditions.length > 0 ? andConditions : undefined
+        },
+        take: limit,
+        skip: skip,
+        orderBy: {
+            [sortBy]: sortOrder
+        },
+        include: {
+            zone:true,
+            feeders:true
+        }
+    })
+
+    const totalZoneCount = await prisma.substation.count({
+        where: {
+            AND: andConditions
+        }
+    })
+
+
+    return {
+        data: allSubstation,
+        meta: {
+            page,
+            limit,
+            total: totalZoneCount,
+            totalPages: Math.ceil(totalZoneCount / limit)
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
 
 export const SubstationService={
-    createSubstation
+    createSubstation,
+    getAllSubstationFromDb
 }
--- src/app/modules/distributionInfrastructure/zone/zone.service.ts
+++ src/app/modules/distributionInfrastructure/zone/zone.service.ts
@@ -9,145 +9,145 @@ import type { ZoneWhereInput } from "../../../../generated/prisma/models";
 import { InfrastructureStatus } from "../../../../generated/prisma/enums";
 
 const createZoneInDb = async (
-  payload: ICreateZonePayload,
-  zoneImageFile: Express.Multer.File
+    payload: ICreateZonePayload,
+    zoneImageFile: Express.Multer.File
 ) => {
-  const existingZoneCode = await prisma.zone.findUnique({
-    where: {
-      code: payload.code,
-    },
-  });
+    const existingZoneCode = await prisma.zone.findUnique({
+        where: {
+            code: payload.code,
+        },
+    });
 
-  if (existingZoneCode) {
-    throw new AppError(
-      httpStatus.CONFLICT,
-      "Zone code already exists"
-    );
-  }
-
-  const { name, code, description, status } = payload;
-
-  const zoneImageUploadResult = await new Promise<UploadApiResponse>(
-    (resolve, reject) => {
-      cloudinary.uploader
-        .upload_stream(
-          {
-            resource_type: "image",
-          },
-          (error, result) => {
-            if (error) {
-              return reject(error);
-            }
-
-            if (!result) {
-              return reject(
-                new AppError(
-                  httpStatus.INTERNAL_SERVER_ERROR,
-                  "No result returned from Cloudinary"
-                )
-              );
-            }
-
-            resolve(result);
-          }
-        )
-        .end(zoneImageFile.buffer);
+    if (existingZoneCode) {
+        throw new AppError(
+            httpStatus.CONFLICT,
+            "Zone code already exists"
+        );
     }
-  );
 
-  const zoneResult = await prisma.zone.create({
-    data: {
-      name,
-      code,
-      description,
-      status,
-      zoneImageUrl:zoneImageUploadResult.secure_url,
-      zoneImagePublicId:zoneImageUploadResult.public_id
-    },
-    include: {
-      substations: true,
-    },
-  });
+    const { name, code, description, status } = payload;
 
-  return zoneResult;
+    const zoneImageUploadResult = await new Promise<UploadApiResponse>(
+        (resolve, reject) => {
+            cloudinary.uploader
+                .upload_stream(
+                    {
+                        resource_type: "image",
+                    },
+                    (error, result) => {
+                        if (error) {
+                            return reject(error);
+                        }
+
+                        if (!result) {
+                            return reject(
+                                new AppError(
+                                    httpStatus.INTERNAL_SERVER_ERROR,
+                                    "No result returned from Cloudinary"
+                                )
+                            );
+                        }
+
+                        resolve(result);
+                    }
+                )
+                .end(zoneImageFile.buffer);
+        }
+    );
+
+    const zoneResult = await prisma.zone.create({
+        data: {
+            name,
+            code,
+            description,
+            status,
+            zoneImageUrl: zoneImageUploadResult.secure_url,
+            zoneImagePublicId: zoneImageUploadResult.public_id
+        },
+        include: {
+            substations: true,
+        },
+    });
+
+    return zoneResult;
 };
 
 
 
 //get all zone public route this is
 
-const getAllZoneFromDb=async(query: IQuery)=>{
+const getAllZoneFromDb = async (query: IQuery) => {
     const limit = query.limit ? Number(query.limit) : 10;
-	const page = query.page ? Number(query.page) : 1;
-	const skip = (page - 1) * limit;
-	const sortBy = query.sortBy ? query.sortBy : "createdAt";
-	const sortOrder = query.sortOrder ? query.sortOrder : "desc"
+    const page = query.page ? Number(query.page) : 1;
+    const skip = (page - 1) * limit;
+    const sortBy = query.sortBy ? query.sortBy : "createdAt";
+    const sortOrder = query.sortOrder ? query.sortOrder : "desc"
 
 
-	const andConditions: ZoneWhereInput[] = []
+    const andConditions: ZoneWhereInput[] = []
 
 
-	//Searching
-	if (query.searchTerm) {
-		andConditions.push({
-			OR: [
-				{ name: { contains: query.searchTerm, mode: "insensitive" } },
-				{ code: { contains: query.searchTerm, mode: "insensitive" } },
-				{
-					description: {
-						contains: query.searchTerm,
-						mode: "insensitive",
-					},
-				},
-				
-			],
-		});
-	}
+    //Searching
+    if (query.searchTerm) {
+        andConditions.push({
+            OR: [
+                { name: { contains: query.searchTerm, mode: "insensitive" } },
+                { code: { contains: query.searchTerm, mode: "insensitive" } },
+                {
+                    description: {
+                        contains: query.searchTerm,
+                        mode: "insensitive",
+                    },
+                },
 
-	//filtering
-	if (query.code) {
-		andConditions.push({
-			code: { equals: query.code, mode: "insensitive" },
-		});
-	}
+            ],
+        });
+    }
 
-	if (query.status) {
-		andConditions.push({
-			status: query.status as InfrastructureStatus,
-		});
-	}
+    //filtering
+    if (query.code) {
+        andConditions.push({
+            code: { equals: query.code, mode: "insensitive" },
+        });
+    }
+
+    if (query.status) {
+        andConditions.push({
+            status: query.status as InfrastructureStatus,
+        });
+    }
 
 
-const allZones=await prisma.zone.findMany({
-where:{
-    AND:andConditions.length >0 ? andConditions : undefined
-},
-		take: limit,
-		skip: skip,
+    const allZones = await prisma.zone.findMany({
+        where: {
+            AND: andConditions.length > 0 ? andConditions : undefined
+        },
+        take: limit,
+        skip: skip,
         orderBy: {
-			[sortBy]: sortOrder
-		},
-        include:{
-            substations:true
+            [sortBy]: sortOrder
+        },
+        include: {
+            substations: true
         }
-})
+    })
 
-const totalZoneCount=await prisma.zone.count({
-    where:{
-        AND:andConditions
+    const totalZoneCount = await prisma.zone.count({
+        where: {
+            AND: andConditions
+        }
+    })
+
+
+    return {
+        data: allZones,
+        meta: {
+            page,
+            limit,
+            total: totalZoneCount,
+            totalPages: Math.ceil(totalZoneCount / limit)
+        }
     }
-})
-
-
-return {
-    data:allZones,
-    meta:{
-        page,
-        limit,
-        total:totalZoneCount,
-        totalPages:Math.ceil(totalZoneCount/limit)
-    }
-}
 
 
 
@@ -160,14 +160,14 @@ return {
 
 //get zone details public route
 
-const getZoneDetails=async(zoneId:string)=>{
-    const zoneDetails=await prisma.zone.findUnique({
-        where:{
-            id:zoneId
+const getZoneDetails = async (zoneId: string) => {
+    const zoneDetails = await prisma.zone.findUnique({
+        where: {
+            id: zoneId
         },
         include:
         {
-            substations:true
+            substations: true
         }
     })
     return zoneDetails
@@ -177,7 +177,7 @@ const getZoneDetails=async(zoneId:string)=>{
 
 
 export const ZoneService = {
-  createZoneInDb,
-  getAllZoneFromDb,
-  getZoneDetails
+    createZoneInDb,
+    getAllZoneFromDb,
+    getZoneDetails
 };
