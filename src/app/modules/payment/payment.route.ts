d0c68698201d87c9f7513b8b0e1dfe397d8c5faf
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-09-05 11:08:16 +0600

    feat: add payment routes for creating, confirming, and retrieving payment details

--- src/app/modules/payment/payment.route.ts
+++ src/app/modules/payment/payment.route.ts
@@ -1,5 +1,24 @@
 import { Router } from "express";
+import { auth } from "../../middlewares/checkAuth";
+import { Role } from "../../../generated/prisma/enums";
 
 const router = Router();
+
+
+//post create payment
+router.post("/create",auth(Role.CUSTOMER),paymentController.createPayment)
+
+
+//payment confirm ssl
+router.post("/confirm",paymentController.verifySslCommerzPayment)
+
+
+//get payment users history
+router.get("/",auth(Role.CUSTOMER),paymentController.getUsersPaymentHistory)
+
+
+//get payment details
+router.get("/:id",auth(Role.CUSTOMER,Role.ADMIN),paymentController.getPaymentDetails)
+
 
 export const PaymentRoutes = router;
