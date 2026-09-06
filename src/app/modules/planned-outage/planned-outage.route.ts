5a40bf767144089987186a4410b0e24c47736563
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-09-06 16:34:58 +0600

    fix: update authorization for planned outage details route to include admin role

--- src/app/modules/planned-outage/planned-outage.route.ts
+++ src/app/modules/planned-outage/planned-outage.route.ts
@@ -15,7 +15,7 @@ router.post("/",auth(Role.ADMIN),validateRequest(plannedOutageValidation.createP
 router.get("/",PlannedOutageController.getAllPlannedOutageSchdeule)
 
 //-3 get planned outage details customer only
-router.get("/:plannedOutageId",auth(Role.CUSTOMER),PlannedOutageController.getAllPlannedOutageSchdeule)
+router.get("/:plannedOutageId",auth(Role.CUSTOMER,Role.ADMIN),PlannedOutageController.getAllPlannedOutageSchdeule)
 
 
 //-4 more update status overall will done later for admin accoridong to business logic
