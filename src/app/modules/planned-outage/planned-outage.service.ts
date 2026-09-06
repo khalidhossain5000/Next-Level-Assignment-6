581070d536ece18651427b8fdbeb4d5b57fdbd94
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-09-06 11:51:38 +0600

    refactor: streamline createPlannedOutageInDb function and improve error handling

--- src/app/modules/planned-outage/planned-outage.service.ts
+++ src/app/modules/planned-outage/planned-outage.service.ts
@@ -1,203 +1,182 @@
-
 import type { IQuery } from "../../interfaces/interface";
 import { prisma } from "../../lib/prisma";
 import { AppError } from "../../utils/AppError";
 
-import httpStatus from "http-status"
-import type { IPlannedOutagePayload, IPlannedOutageUpdatePayload } from "./planned-outage.interface";
+import httpStatus from "http-status";
+import type {
+  IPlannedOutagePayload,
+  IPlannedOutageUpdatePayload,
+} from "./planned-outage.interface";
 import type { PlannedOutageWhereInput } from "../../../generated/prisma/models";
 import type { PlannedOutageStatus } from "../../../generated/prisma/enums";
 import { Prisma } from "../../../generated/prisma/client";
 
 //create planned outage
 
-const createPlannedOutageInDb = async (
-    payload: IPlannedOutagePayload
-) => {
-    const {title,areaId,description,endTime,reason,startTime} = payload;
+const createPlannedOutageInDb = async (payload: IPlannedOutagePayload) => {
+  const { title, areaId, description, endTime, reason, startTime } = payload;
 
-    // 1. Validate time range
-    if (startTime >= endTime) {
-        throw new AppError(
-            httpStatus.BAD_REQUEST,
-            "Start time must be before end time"
-        );
-    }
+  // 1. Validate time range
+  if (startTime >= endTime) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "Start time must be before end time"
+    );
+  }
 
-    // 2. Start time cannot be in the past
-    if (startTime < new Date()) {
-        throw new AppError(
-            httpStatus.BAD_REQUEST,
-            "Load shedding schedule cannot start in the past"
-        );
-    }
+  // 2. Start time cannot be in the past
+  if (startTime < new Date()) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "Load shedding schedule cannot start in the past"
+    );
+  }
 
-    // 3. need to if Check area exists and is active
-    const area = await prisma.area.findUnique({
-        where: {
-            id: areaId,
-        },
-        select: {
-            id: true,
-            status: true,
-        },
-    });
+  // 3. need to if Check area exists and is active
+  const area = await prisma.area.findUnique({
+    where: {
+      id: areaId,
+    },
+    select: {
+      id: true,
+      status: true,
+    },
+  });
 
-    if (!area) {
-        throw new AppError(
-            httpStatus.NOT_FOUND,
-            "Area not found"
-        );
-    }
+  if (!area) {
+    throw new AppError(httpStatus.NOT_FOUND, "Area not found");
+  }
 
-    if (area.status !== "ACTIVE") {
-        throw new AppError(
-            httpStatus.BAD_REQUEST,
-            "Cannot create load shedding schedule for an inactive area"
-        );
-    }
+  if (area.status !== "ACTIVE") {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "Cannot create load shedding schedule for an inactive area"
+    );
+  }
 
-    // 4. Check plannedOutage  conflict in the same area
-    const plannedOutageConflict =
-        await prisma.plannedOutage.findFirst({
-            where: {
-                areaId,
+  // 4. Check plannedOutage  conflict in the same area
+  const plannedOutageConflict = await prisma.plannedOutage.findFirst({
+    where: {
+      areaId,
 
-                // Ignore all cancelled schedules
-                status: {
-                    not: "CANCELLED",
-                },
+      // Ignore all cancelled schedules
+      status: {
+        not: "CANCELLED",
+      },
 
+      startTime: {
+        lt: endTime,
+      },
 
-                startTime: {
-                    lt: endTime,
-                },
+      endTime: {
+        gt: startTime,
+      },
+    },
+  });
 
-                endTime: {
-                    gt: startTime,
-                },
-            },
-        });
+  if (plannedOutageConflict) {
+    throw new AppError(
+      httpStatus.CONFLICT,
+      "A Planned Outage schedule already exists for this area during the selected time"
+    );
+  }
 
-    if (plannedOutageConflict) {
-        throw new AppError(
-            httpStatus.CONFLICT,
-            "A Planned Outage schedule already exists for this area during the selected time"
-        );
-    }
+  // 5. Create schedule
+  const createdSchedule = await prisma.plannedOutage.create({
+    data: {
+      title,
+      startTime,
+      endTime,
+      description,
+      reason,
+      areaId,
+    },
+  });
 
-    // 5. Create schedule
-    const createdSchedule =
-        await prisma.plannedOutage.create({
-            data: {
-                title,
-                startTime,
-                endTime,
-                description,
-                reason,
-                areaId,
-            },
-        });
-
-    return createdSchedule;
+  return createdSchedule;
 };
-
 
 //get all get All Planned Outage schedule
 
 const getAllPlannedOutage = async (query: IQuery) => {
-    const limit = query.limit ? Number(query.limit) : 10;
-    const page = query.page ? Number(query.page) : 1;
-    const skip = (page - 1) * limit;
-    const sortBy = query.sortBy ? query.sortBy : "createdAt";
-    const sortOrder = query.sortOrder ? query.sortOrder : "desc"
+  const limit = query.limit ? Number(query.limit) : 10;
+  const page = query.page ? Number(query.page) : 1;
+  const skip = (page - 1) * limit;
+  const sortBy = query.sortBy ? query.sortBy : "createdAt";
+  const sortOrder = query.sortOrder ? query.sortOrder : "desc";
 
+  const andConditions: PlannedOutageWhereInput[] = [];
 
-    const andConditions: PlannedOutageWhereInput[] = []
-
-
-    //Searching
-    if (query.searchTerm) {
-        andConditions.push({
-            OR: [
-                { title: { contains: query.searchTerm, mode: "insensitive" } },
-                {
-                    reason: {
-                        contains: query.searchTerm,
-                        mode: "insensitive",
-                    },
-                },
-                 {
-                    description: {
-                        contains: query.searchTerm,
-                        mode: "insensitive",
-                    },
-                },
-
-            ],
-        });
-    }
-
-
-
-    if (query.status) {
-        andConditions.push({
-            status: query.status as PlannedOutageStatus,
-        });
-    }
-
-
-    const allPlannedOutageSchedule = await prisma.plannedOutage.findMany({
-        where: {
-            AND: andConditions.length > 0 ? andConditions : undefined
+  //Searching
+  if (query.searchTerm) {
+    andConditions.push({
+      OR: [
+        { title: { contains: query.searchTerm, mode: "insensitive" } },
+        {
+          reason: {
+            contains: query.searchTerm,
+            mode: "insensitive",
+          },
         },
-        take: limit,
-        skip: skip,
-        orderBy: {
-            [sortBy]: sortOrder
+        {
+          description: {
+            contains: query.searchTerm,
+            mode: "insensitive",
+          },
         },
-        include: {
-            area: true,
+      ],
+    });
+  }
 
-        }
-    })
+  if (query.status) {
+    andConditions.push({
+      status: query.status as PlannedOutageStatus,
+    });
+  }
 
-    const totalPlannedOutageCount = await prisma.plannedOutage.count({
-        where: {
-            AND: andConditions
-        }
-    })
+  const allPlannedOutageSchedule = await prisma.plannedOutage.findMany({
+    where: {
+      AND: andConditions.length > 0 ? andConditions : undefined,
+    },
+    take: limit,
+    skip: skip,
+    orderBy: {
+      [sortBy]: sortOrder,
+    },
+    include: {
+      area: true,
+    },
+  });
 
+  const totalPlannedOutageCount = await prisma.plannedOutage.count({
+    where: {
+      AND: andConditions,
+    },
+  });
 
-    return {
-        data: allPlannedOutageSchedule,
-        meta: {
-            page,
-            limit,
-            total: totalPlannedOutageCount,
-            totalPages: Math.ceil(totalPlannedOutageCount / limit)
-        }
-    }
-
-}
-
+  return {
+    data: allPlannedOutageSchedule,
+    meta: {
+      page,
+      limit,
+      total: totalPlannedOutageCount,
+      totalPages: Math.ceil(totalPlannedOutageCount / limit),
+    },
+  };
+};
 
 //get planned outage details
 const plannedOutageDetails = async (plannedOutageId: string) => {
-    const getDetails = await prisma.plannedOutage.findUniqueOrThrow({
-        where: {
-            id: plannedOutageId
-        },
-        include:{
-            area:true
-        }
-    })
-    return getDetails
-}
-
-
-
-
+  const getDetails = await prisma.plannedOutage.findUniqueOrThrow({
+    where: {
+      id: plannedOutageId,
+    },
+    include: {
+      area: true,
+    },
+  });
+  return getDetails;
+};
 
 //update load shedding scheudle
 
@@ -220,7 +199,7 @@ const updatePlannedOutage = async (
   }
 
   // 2. Get final values
- 
+
   const finalStartTime = payload.startTime ?? existingSchedule.startTime;
   const finalEndTime = payload.endTime ?? existingSchedule.endTime;
 
@@ -233,11 +212,8 @@ const updatePlannedOutage = async (
   }
 
   // 4. Start time cannot be in the past checkh ere
- 
-  if (
-    payload.startTime &&
-    finalStartTime < new Date()
-  ) {
+
+  if (payload.startTime && finalStartTime < new Date()) {
     throw new AppError(
       httpStatus.BAD_REQUEST,
       "Load shedding schedule cannot start in the past"
@@ -256,10 +232,7 @@ const updatePlannedOutage = async (
   });
 
   if (!area) {
-    throw new AppError(
-      httpStatus.NOT_FOUND,
-      "Area not found"
-    );
+    throw new AppError(httpStatus.NOT_FOUND, "Area not found");
   }
 
   if (area.status !== "ACTIVE") {
@@ -270,28 +243,26 @@ const updatePlannedOutage = async (
   }
 
   // 6. Check conflict with other schedules in same area or not
-  const conflictingSchedule =
-    await prisma.plannedOutage.findFirst({
-      where: {
-        areaId: existingSchedule.areaId,
+  const conflictingSchedule = await prisma.plannedOutage.findFirst({
+    where: {
+      areaId: existingSchedule.areaId,
 
-
-        id: {
-          not: planeedOutageId,
-        },
-
-        status: {
-          not: "CANCELLED",
-        },
-
-        startTime: {
-          lt: finalEndTime,
-        },
-        endTime: {
-          gt: finalStartTime,
-        },
+      id: {
+        not: planeedOutageId,
       },
-    });
+
+      status: {
+        not: "CANCELLED",
+      },
+
+      startTime: {
+        lt: finalEndTime,
+      },
+      endTime: {
+        gt: finalStartTime,
+      },
+    },
+  });
 
   if (conflictingSchedule) {
     throw new AppError(
@@ -319,7 +290,6 @@ const updatePlannedOutage = async (
     updateData.reason = payload.reason;
   }
 
-
   // 8. Update
   const updatedSchedule = await prisma.plannedOutage.update({
     where: {
@@ -331,11 +301,9 @@ const updatePlannedOutage = async (
   return updatedSchedule;
 };
 
-
-
 export const PlannedOutageService = {
-    createPlannedOutageInDb,
-    getAllPlannedOutage,
-    plannedOutageDetails,
-    updatePlannedOutage
-}
+  createPlannedOutageInDb,
+  getAllPlannedOutage,
+  plannedOutageDetails,
+  updatePlannedOutage,
+};
