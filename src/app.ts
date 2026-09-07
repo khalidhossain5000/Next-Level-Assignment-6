b4c15f5bb9008ae64c822dbc1ead415c2b65e91c
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-09-07 10:24:00 +0600

    feat: add analytics service route to the application

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,7 @@ import { LoadSheddingRoutes } from "./app/modules/loadshedding/load-shedding.rou
 import { PaymentRoutes } from "./app/modules/payment/payment.route";
 import { PlannedOutageRoutes } from "./app/modules/planned-outage/planned-outage.route";
 import { AdminRoutes } from "./app/modules/admin/admin.route";
+import { AnalyticsRoutes } from "./app/modules/analytics/analytics.service";
 const app: Application = express();
 
 //cors setup
@@ -68,7 +69,9 @@ app.use("/api/v1/planned-outage", PlannedOutageRoutes)
 app.use("/api/v1/admin", AdminRoutes)
 
 
-//anal
+//analytics report reated
+
+app.use("/api/v1/analytics",AnalyticsRoutes)
 
 
 
