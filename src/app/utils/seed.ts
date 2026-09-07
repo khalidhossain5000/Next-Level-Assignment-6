be2ce5a58a7905e08643dca53a352754465434d9
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-09-07 11:25:19 +0600

    feat: add seeding function for tester technician account with profile creation

--- src/app/utils/seed.ts
+++ src/app/utils/seed.ts
@@ -1,4 +1,4 @@
-import { Role } from "../../generated/prisma/enums";
+import { Role, TechnicianProfileStatus, TechnicianStatus } from "../../generated/prisma/enums";
 import config from "../config";
 import { prisma } from "../lib/prisma";
 import bcrypt from "bcryptjs";
@@ -51,7 +51,64 @@ export const seedTesterAdmin = async () => {
 
 //seed default technician account
 
+export const seedTesterTechnician = async () => {
+  try {
+    const result = await prisma.$transaction(async (tx) => {
+      // -- Check technician account already exists
+      const existingTechnician = await tx.user.findUnique({
+        where: {
+          email: config.tester_technician_email,
+        },
+      });
 
+      if (existingTechnician) {
+        console.log("Tester technician already exists with this email");
+        return null;
+      }
+
+      // --Hash password
+      const hashedPassword = await bcrypt.hash(
+        config.tester_technician_password as string,
+        Number(config.bcrypt_salt_rounds),
+      );
+
+      // --Create technician account
+      const technician = await tx.user.create({
+        data: {
+          name: config.tester_technician_name,
+          email: config.tester_technician_email,
+          password: hashedPassword,
+          role: Role.TECHNICIAN,
+          emailVerified: true,
+        },
+      });
+
+      // -- Create technician profile
+      const technicianProfile = await tx.technicianProfile.create({
+        data: {
+          userId: technician.id,
+          expertise: ["Electrical Maintenance", "Power Distribution"],
+          experience: 1,
+          availability: TechnicianStatus.AVAILABLE,
+          bio: "Tester technician profile",
+          technicianProfileStatus: TechnicianProfileStatus.APPROVED,
+        },
+      });
+
+      return {
+        technician,
+        technicianProfile,
+      };
+    });
+
+    if (!result) return;
+
+    console.log("Tester technician account created:", result.technician);
+    console.log("Tester technician profile created:", result.technicianProfile);
+  } catch (error) {
+    console.log(error, "Error while tester technician seeding");
+  }
+};
 
 
 
