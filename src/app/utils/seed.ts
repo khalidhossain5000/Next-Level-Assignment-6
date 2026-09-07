1ea10e704bacca026c0f1e9b9fdf00e35db07448
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-09-07 13:38:10 +0600

    fix: remove unnecessary console log in seedTesterAdmin function

--- src/app/utils/seed.ts
+++ src/app/utils/seed.ts
@@ -6,7 +6,7 @@ import bcrypt from "bcryptjs";
 
 export const seedTesterAdmin = async () => {
 	try {
-		console.log(config.tes)
+		
 		//1.need to check user exist or not
 		const isTesterAdmin = await prisma.user.findUnique({
 			where: {
